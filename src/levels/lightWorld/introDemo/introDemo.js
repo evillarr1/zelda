5e34ecb7d73524538988c127a28ca96703f1fdc1
Edgar Villarreal <evillarr1@gmail.com>
2016-06-26 20:55:43 -0700

    Fixing characters template and adding introDemo design

--- src/levels/lightWorld/introDemo/introDemo.js
+++ src/levels/lightWorld/introDemo/introDemo.js
@@ -25,17 +25,330 @@ export default class IntroDemo {
 
 	draw() {
 		Context.clearRect(0, 0, Canvas.width, Canvas.height);
+		this.leftWall();
+		this.bottomWall();
+		this.topWall();
+	}
 
-		// Draw the background
+	topWall() {
+		// Draw left wall
 		Context.drawImage(
 			this.charSelectSheet,
-			264,
-			1,
-			Constants.GAME_WIDTH,
-			Constants.GAME_HEIGHT,
+			24,
+			88,
+			16,
+			24,
+			24,
+			28,
+			16,
+			24);
+
+		Context.drawImage(
+			this.charSelectSheet,
+			24,
+			88,
+			16,
+			24,
+			40,
+			28,
+			16,
+			24);
+
+		Context.drawImage(
+			this.charSelectSheet,
+			24,
+			88,
+			16,
+			24,
+			56,
+			28,
+			16,
+			24);
+
+		Context.drawImage(
+			this.charSelectSheet,
+			24,
+			88,
+			16,
+			24,
+			72,
+			28,
+			16,
+			24);
+
+		Context.drawImage(
+			this.charSelectSheet,
+			24,
+			88,
+			16,
+			24,
+			88,
+			28,
+			16,
+			24);
+
+		Context.drawImage(
+			this.charSelectSheet,
+			24,
+			88,
+			16,
+			24,
+			104,
+			28,
+			16,
+			24);
+
+		Context.drawImage(
+			this.charSelectSheet,
+			24,
+			88,
+			16,
+			24,
+			120,
+			28,
+			16,
+			24);
+
+		Context.drawImage(
+			this.charSelectSheet,
+			24,
+			88,
+			16,
+			24,
+			136,
+			28,
+			16,
+			24);
+
+		Context.drawImage(
+			this.charSelectSheet,
+			24,
+			88,
+			16,
+			24,
+			152,
+			28,
+			16,
+			24);
+
+		Context.drawImage(
+			this.charSelectSheet,
+			24,
+			88,
+			16,
+			24,
+			168,
+			28,
+			16,
+			24);
+
+		Context.drawImage(
+			this.charSelectSheet,
+			24,
+			88,
+			16,
+			24,
+			184,
+			28,
+			16,
+			24);
+
+		// Draw edges
+		Context.drawImage(
+			this.charSelectSheet,
+			190,
+			216,
+			24,
+			24,
 			0,
+			28,
+			24,
+			24);
+
+		Context.drawImage(
+			this.charSelectSheet,
+			215,
+			216,
+			24,
+			24,
+			200,
+			28,
+			24,
+			24);
+
+		// Draw items
+		Context.drawImage(
+			this.charSelectSheet,
+			89,
+			164,
+			16,
+			16,
+			48,
+			28,
+			16,
+			16);
+
+		Context.drawImage(
+			this.charSelectSheet,
+			351,
+			25,
+			48,
+			32,
+			88,
+			28,
+			48,
+			32);
+
+		Context.drawImage(
+			this.charSelectSheet,
+			48,
+			48,
+			32,
+			32,
+			152,
+			28,
+			32,
+			32);
+	}
+
+	leftWall() {
+		// Draw left wall
+		Context.drawImage(
+			this.charSelectSheet,
 			0,
-			Constants.GAME_WIDTH,
-			Constants.GAME_HEIGHT);
+			112,
+			24,
+			16,
+			0,
+			52,
+			24,
+			16);
+
+		Context.drawImage(
+			this.charSelectSheet,
+			0,
+			112,
+			24,
+			16,
+			0,
+			68,
+			24,
+			16);
+
+		Context.drawImage(
+			this.charSelectSheet,
+			0,
+			112,
+			24,
+			16,
+			0,
+			84,
+			24,
+			16);
+
+
+		Context.drawImage(
+			this.charSelectSheet,
+			0,
+			112,
+			24,
+			16,
+			0,
+			100,
+			24,
+			16);
+
+		Context.drawImage(
+			this.charSelectSheet,
+			0,
+			112,
+			24,
+			16,
+			0,
+			134,
+			24,
+			16);
+
+		Context.drawImage(
+			this.charSelectSheet,
+			0,
+			112,
+			24,
+			16,
+			0,
+			150,
+			24,
+			16);
+
+		Context.drawImage(
+			this.charSelectSheet,
+			0,
+			112,
+			24,
+			16,
+			0,
+			166,
+			24,
+			16);
+
+		Context.drawImage(
+			this.charSelectSheet,
+			0,
+			112,
+			24,
+			16,
+			0,
+			182,
+			24,
+			16);
+
+		// Draw edges
+		Context.drawImage(
+			this.charSelectSheet,
+			166,
+			217,
+			24,
+			24,
+			0,
+			28,
+			24,
+			24);
+
+		Context.drawImage(
+			this.charSelectSheet,
+			166,
+			242,
+			24,
+			24,
+			0,
+			198,
+			24,
+			24);
+
+		// Draw items
+		Context.drawImage(
+			this.charSelectSheet,
+			75,
+			101,
+			24,
+			32,
+			0,
+			108,
+			24,
+			32);
+	}
+
+	bottomWall() {
+		// Draw left wall
+		Context.drawImage(
+			this.charSelectSheet,
+			190,
+			243,
+			24,
+			24,
+			0,
+			198,
+			24,
+			24);
 	}
 }
