69746639b5af9cb9c632872b97e6c502f6c3c713
Edgar Villarreal <evillarr1@gmail.com>
2016-07-03 10:02:14 -0700

    Further fine tuned links movements

--- src/core/Player.js
+++ src/core/Player.js
@@ -61,42 +61,28 @@ export default class Player {
 
 		// Find the collisions and the offset of each
 		let collisions = {};
-		let edgeCollision = {};
 		this.mapObjects.neutral.forEach(([posX, posY, x, y]) => {
 			let response = Game.collision("UNIT", [this.xPos, this.yPos + 10, 16, 16], [posX, posY, x, y]);
-			let edgeResponse = Game.collision("UNIT", [this.xPos + 1.5, this.yPos + 11.5, 13, 13], [posX, posY, x, y]);
-
-			if (edgeResponse !== true) {
-				if (edgeResponse.overlapV.y > 0) {
-					edgeCollision.DOWN = edgeResponse.overlapV.y;
-				} else if (edgeResponse.overlapV.y < 0) {
-					edgeCollision.UP = edgeResponse.overlapV.y;
-				} else if (edgeResponse.overlapV.x > 0) {
-					edgeCollision.RIGHT = edgeResponse.overlapV.x;
-				} else if (edgeResponse.overlapV.x < 0) {
-					edgeCollision.LEFT = edgeResponse.overlapV.x;
-				}
-			}
 
 			if (response !== true) {
 				if (response.overlapV.y > 0) {
 					collisions.DOWN = {
-						overlap: response.overlapV.y,
+						overlap: response.overlapV,
 						coordinates: [posX, posY, x, y]
 					};
 				} else if (response.overlapV.y < 0) {
 					collisions.UP = {
-						overlap: response.overlapV.y,
+						overlap: response.overlapV,
 						coordinates: [posX, posY, x, y]
 					};
 				} else if (response.overlapV.x > 0) {
 					collisions.RIGHT = {
-						overlap: response.overlapV.x,
+						overlap: response.overlapV,
 						coordinates: [posX, posY, x, y]
 					};
 				} else if (response.overlapV.x < 0) {
 					collisions.LEFT = {
-						overlap: response.overlapV.x,
+						overlap: response.overlapV,
 						coordinates: [posX, posY, x, y]
 					};
 				}
@@ -112,61 +98,93 @@ export default class Player {
 			if (direction === "DOWN") {
 				if (!collisions.DOWN) {
 					this._moveDown(pos, lenY);
-				} else if (!Object.keys(edgeCollision).length) {
-					if (this.xPos < collisions.DOWN.coordinates[0] - 8) {
-						this._moveLeft(pos, lenX);
-					} else if (this.xPos > collisions.DOWN.coordinates[0] + collisions.DOWN.coordinates[2] - 8) {
-						this._moveRight(pos, lenX);
+				} else {
+					if (this.xPos < collisions.DOWN.coordinates[0] - 4) {
+						this._moveLeft(collisions.DOWN.coordinates[0], lenX, true, collisions.LEFT);
+					} else if (this.xPos > collisions.DOWN.coordinates[0] + collisions.DOWN.coordinates[2] - 10) {
+						this._moveRight(collisions.DOWN.coordinates[0] + collisions.DOWN.coordinates[2], lenX, true, collisions.RIGHT);
 					}
 				}
 			} else if (direction === "UP") {
 				if (!collisions.UP) {
 					this._moveUp(pos, lenY);
-				} else if (!Object.keys(edgeCollision).length) {
-					if (this.xPos < collisions.UP.coordinates[0] - 8) {
-						this._moveLeft(pos, lenX);
-					} else if (this.xPos > collisions.UP.coordinates[0] + collisions.UP.coordinates[2] - 8) {
-						this._moveRight(pos, lenX);
+				} else {
+					if (this.xPos < collisions.UP.coordinates[0] - 4) {
+						this._moveLeft(collisions.UP.coordinates[0], lenX, true, collisions.LEFT);
+					} else if (this.xPos > collisions.UP.coordinates[0] + collisions.UP.coordinates[2] - 10) {
+						this._moveRight(collisions.UP.coordinates[0] + collisions.UP.coordinates[2], lenX, true, collisions.RIGHT);
 					}
 				}
 			} else if (direction === "LEFT") {
 				if (!collisions.LEFT) {
 					this._moveLeft(pos, lenX);
-				} else if (!Object.keys(edgeCollision).length) {
+				} else {
 					if (this.yPos < collisions.LEFT.coordinates[1] - 14) {
-						this._moveUp(pos, lenY);
+						this._moveUp(collisions.LEFT.coordinates[1], lenY, true, collisions.UP);
 					} else if (this.yPos > collisions.LEFT.coordinates[1] + collisions.LEFT.coordinates[3] - 22) {
-						this._moveDown(pos, lenY);
+						this._moveDown(collisions.LEFT.coordinates[1] + collisions.LEFT.coordinates[3], lenY, true, collisions.DOWN);
 					}
 				}
 			} else if (direction === "RIGHT") {
 				if (!collisions.RIGHT) {
 					this._moveRight(pos, lenX);
-				} else if (!Object.keys(edgeCollision).length) {
+				} else {
 					if (this.yPos < collisions.RIGHT.coordinates[1] - 14) {
-						this._moveUp(pos, lenY);
+						this._moveUp(collisions.RIGHT.coordinates[1], lenY, true, collisions.UP);
 					} else if (this.yPos > collisions.RIGHT.coordinates[1] + collisions.RIGHT.coordinates[3] - 22) {
-						this._moveDown(pos, lenY);
+						this._moveDown(collisions.RIGHT.coordinates[1] + collisions.RIGHT.coordinates[3], lenY, true, collisions.DOWN);
 					}
 				}
 			}
 		});
 	};
 
-	_moveDown(pos, lenY) {
-		this.yPos = Number((this.yPos + pos).toPrecision(lenY));
+	_moveDown(pos, lenY, shouldTransform, direction) {
+		if (shouldTransform) {
+			if (!direction) {
+				this.yPos = pos + (Number((this.yPos - pos) + 1));
+			} else {
+				this.yPos = pos + (Number((this.yPos - pos) + direction.overlap.y));
+			}
+		} else {
+			this.yPos = Number((this.yPos + pos).toPrecision(lenY));
+		}
 	}
 
-	_moveUp(pos, lenY) {
-		this.yPos = Number((this.yPos - pos).toPrecision(lenY));
+	_moveUp(pos, lenY, shouldTransform, direction) {
+		if (shouldTransform) {
+			if (!direction) {
+				this.yPos = pos - (Number((pos - this.yPos) + 1));
+			} else {
+				this.yPos = pos - (Number((pos - this.yPos) + direction.overlap.y));
+			}
+		} else {
+			this.yPos = Number((this.yPos - pos).toPrecision(lenY));
+		}
 	}
 
-	_moveLeft(pos, lenX) {
-		this.xPos = Number((this.xPos - pos).toPrecision(lenX));
+	_moveLeft(pos, lenX, shouldTransform, direction) {
+		if (shouldTransform) {
+			if (!direction) {
+				this.xPos = pos - (Number((pos - this.xPos) + 1));
+			} else {
+				this.xPos = pos - (Number((pos - this.xPos) + direction.overlap.x));
+			}
+		} else {
+			this.xPos = Number((this.xPos - pos).toPrecision(lenX));
+		}
 	}
 
-	_moveRight(pos, lenX) {
-		this.xPos = Number((this.xPos + pos).toPrecision(lenX))
+	_moveRight(pos, lenX, shouldTransform, direction) {
+		if (shouldTransform) {
+			if (!direction) {
+				this.xPos = pos + (Number((this.xPos - pos) + 1));
+			} else {
+				this.xPos = pos - (Number((pos - this.xPos) + direction.overlap.x));
+			}
+		} else {
+			this.xPos = Number((this.xPos + pos).toPrecision(lenX))
+		}
 	}
 
 	setLevelObjects(mapObjects) {
